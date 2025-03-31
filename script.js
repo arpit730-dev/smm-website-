4437c9269945cfd74e6d22031742a0cea295693e
arpit730-dev <ap9841911@gmail.com>
2025-03-31 19:09:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Dark Mode Toggle
+const themeToggle = document.getElementById('themeToggle');
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  // Change icon
+  const icon = themeToggle.querySelector('i');
+  if (document.body.classList.contains('dark-mode')) {
+    icon.classList.replace('fa-moon', 'fa-sun');
+  } else {
+    icon.classList.replace('fa-sun', 'fa-moon');
+  }
+});
+
+// Update service icons HTML (replace old icons)
+document.querySelectorAll('.fa-twitter').forEach(icon => {
+  icon.classList.replace('fa-twitter', 'fa-x-twitter');
+});
+
 // Mobile Menu Toggle
 document.querySelector('.mobile-menu').addEventListener('click', function() {
   const nav = document.querySelector('nav');
